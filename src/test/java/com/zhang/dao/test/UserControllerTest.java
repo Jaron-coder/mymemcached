a3c184cf8f4a615790b9fedb6495f69341292158
zhanglong <zhang_long_zl@126.com>
2015-06-15 17:22:35 +0800

    aa

--- src/test/java/com/zhang/dao/test/UserControllerTest.java
+++ src/test/java/com/zhang/dao/test/UserControllerTest.java
@@ -10,8 +10,13 @@ import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 /**
  * Created by zhanglong on 2015/6/1.
@@ -29,12 +34,13 @@ public class UserControllerTest {
         mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
     }
 
-//    @Test
-//    public void getHello() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
-//    }
+    @Test
+    public void getHello() throws Exception {
+        ResultActions r= mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+        System.out.println(r.toString());
+    }
 
 
 }
